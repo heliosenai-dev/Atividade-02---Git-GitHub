7674d662bdb5c6bc418dc2e2db85c0c4219d85d0
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 20:04:47 -0300

    Comandos Console adicionados

--- js/script_fundamentos.js
+++ js/script_fundamentos.js
@@ -8,3 +8,26 @@
 
  //COMANDO DE SAÍDA
  console.log("Olá mundo !giu")
+//DECLARAÇÃO DE VARIÁVEL
+let num = 10
+var num2 = 50
+const num3 = 100
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 250
+console.log(num4)
+var num5 = 140
+}
+console.log(num5)
+
+num = 1882
+
+console.log(num)
+
+num2 = 58
+
+console.log(num2)
+
